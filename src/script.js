847175438b7c4ba5585a395ddb696035a1b0e84b
Kriti Gupta <kritig4@gmail.com>
2024-03-15 19:29:00 -0500

    Add timezone converter

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function convertTime() {
+    const datetime = document.getElementById('datetime').value;
+    const sourceTimezone = document.getElementById('sourceTimezone').value;
+    const targetTimezone = document.getElementById('targetTimezone').value;
+
+    const sourceDatetime = moment.tz(datetime, sourceTimezone);
+    const targetDatetime = sourceDatetime.clone().tz(targetTimezone);
+
+    document.getElementById('result').innerText = `Converted Date/Time: ${targetDatetime.format('YYYY-MM-DD HH:mm:ss')}`;
+}
