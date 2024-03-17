663814cdafd5e61a11d14ef1e11f8fe15b880b5a
Kriti Gupta <kritig4@gmail.com>
2024-03-16 22:14:51 -0500

    Add list of timezones

--- script.js
+++ script.js
@@ -11,6 +11,23 @@ function convertTime() {
 
 flatpickr("#datetime", {
     enableTime: true,
-    dateFormat: "Y-m-d H:i:S",
-    allowInput: true
+    dateFormat: "Y-m-d H:i:S"
 });
+
+function showSelected() {
+    const selectedTimezone = document.getElementById('sourceTimezone').value;
+}
+
+function filterTimezones() {
+    const input = document.getElementById('searchInput').value.toUpperCase();
+    const options = document.getElementById('sourceTimezone').options;
+
+    for (let i = 0; i < options.length; i++) {
+        const optionText = options[i].text.toUpperCase();
+        if (optionText.indexOf(input) > -1) {
+            options[i].style.display = '';
+        } else {
+            options[i].style.display = 'none';
+        }
+    }
+}
